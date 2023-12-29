6bd6bdb0c5a9942c7a4a21c4cacd63c746f0a617
Emil Roydev <e.roydev@gmail.com>
2023-12-28 17:09:59 -0800

    playground

---
+++ Components/components-basics/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MovieList from './components/MovieList'
+import movies from './assets/movies'
+
+function App() {
+  return (
+    <div>
+      <h1>My first Dynamic React Application</h1>
+      <MovieList headingText="Movie List" movies={movies}/>
+    </div>
+  )
+}
+
+export default App
