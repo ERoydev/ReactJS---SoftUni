8a1f7c1987b43d33707d29d894bf421252f878b2
Emil Roydev <e.roydev@gmail.com>
2024-01-05 19:58:36 -0800

    changes

--- Components/components-deep dive/src/App.jsx
+++ Components/components-deep dive/src/App.jsx
@@ -1,17 +1,22 @@
-import './App.css'
+import styles from './App.module.css';
 import { useState, useEffect } from 'react';
+import Starwars from './Starwars';
 
 function App() {
   const [numbers, setNumbers] = useState([1, 2, 3, 4, 5]);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     console.log('Mount component');
-    
   }, [])
 
   useEffect(() => {
-    console.log('Update component - numbers')
+    console.log(`Update component - ${numbers.length}`)
   }, [numbers])
+
+  useEffect(() => {
+    setTimeout(() => setCount(s => s + 1), 1000)
+  }, [count]);
 
   const onClick = () => {
     setNumbers(oldState => oldState.slice(0, oldState.length - 1));
@@ -22,11 +27,23 @@ function App() {
   }
 
   return (
-    <div>
+    <div className={styles.app}>
+      
+      <Starwars />
+
+      <h3>Count: {count}</h3>
         <ul>
-        {numbers.map((number, index) => <li key={index}>{number * 2}</li>)}
+        {numbers.map((number, index) =>  (
+        <li 
+          key={index}
+          className={styles['listItem']}
+          >
+            {number * 2}
+          </li>)
+          )}
       </ul>
       <button onClick={onClick}>Remove</button>
+      <button onClick={() => setCount(c => c + 1)}>+</button>
     </div>
   );
 }
