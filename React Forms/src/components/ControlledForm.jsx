f6be69e00040902233cc2c1dbedec928e2d70735
Emil Roydev <e.roydev@gmail.com>
2024-01-13 08:02:19 -0800

    controled, uncotroled forms

--- React Forms/src/components/ControlledForm.jsx
+++ React Forms/src/components/ControlledForm.jsx
@@ -1,6 +1,32 @@
+import { useState } from "react";
 
 export default function ControlledForm() {
 
+    const [usernameValue, setUsernameValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+    const [ageValue, setAgeValue] = useState('');
+    
+    const usernameChangeHandler = (e) => {
+        setUsernameValue(e.target.value);
+    }
+
+    const passwordChangeHandler = (e) => {
+        setPasswordValue(e.target.value);
+    }
+
+    const ageChangeHandler = (e) => {
+        setAgeValue(e.target.value);
+    }
+
+    const resetFormHandler = () => {
+        setUsernameValue('');
+        setPasswordValue('');
+        setAgeValue('');
+    }
+
+    const usernameBlurHandler = () => {
+        console.log('on blur')
+    }
 
     return (
         <>
@@ -9,18 +35,32 @@ export default function ControlledForm() {
             <form>
                 <div>
                     <label htmlFor="username">Username:</label>
-                    <input type="text" name="username" id="username" />
+                    <input type="text"
+                     name="username"
+                     id="username" 
+                     value={usernameValue}
+                     onChange={usernameChangeHandler} 
+                     onBlur={usernameBlurHandler}/>
                 </div>
                 <div>
                     <label htmlFor="username">Password:</label>
-                    <input type="password" name="password" id="password" />
+                    <input type="password"
+                     name="password"
+                     id="password"
+                     value={passwordValue}
+                     onChange={passwordChangeHandler}/>
                 </div>
                 <div>
                     <label htmlFor="username">Age:</label>
-                    <input type="number" name="age" id="age" />
+                    <input type="number"
+                     name="age"
+                     id="age" 
+                     value={ageValue}
+                     onChange={ageChangeHandler}/>
                 </div>
                 <div>
-                    <input type="submit" value="Register" />
+                    <button>Register</button>
+                    <button type="button" onClick={resetFormHandler}>Reset</button>
                 </div>
             </form>
         </>
