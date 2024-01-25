0d3c6e6248ef318d1bf797bc50b72c6e989b28cc
Emil Roydev <e.roydev@gmail.com>
2024-01-25 07:42:47 -0800

    auth model

---
+++ Js Applications-Softuni/Data and Authentication - Exercise/06.Furniture/login.js
@@ -0,0 +1,89 @@
+async function registerUser(email, password) {
+  const baseUrl = 'http://localhost:3030/users/register';
+  const response = await fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    })
+  })
+
+  const data = await response.json();
+  return data;
+};
+
+async function loginUser(email, password) {
+    const baseUrl = 'http://localhost:3030/users/login';
+
+    const response = await fetch(baseUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+            password, password,
+        })
+    })
+
+    if(response.status == 200) {
+        const data = await response.json();
+        return data;
+    } else {
+        throw new Error('Such user dont exists.');
+    };
+
+};
+
+function solve() {
+  const registerForm = document.querySelector('#registerForm');
+  const loginForm = document.querySelector('#loginForm');
+
+  registerForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(registerForm);
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const rePass = formData.get('rePass');
+
+
+    if (!email || !password || !rePass || passwod !== rePass) {
+      throw new Error("missing fields or wrong password");
+
+    } else {
+      const data = await registerUser(email, password);
+      sessionStorage.setItem("token", data.accessToken);
+      sessionStorage.setItem("userId", data._id);
+         
+      window.location.href="./homeLogged.html";
+    }
+  })
+
+  loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(loginForm);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    console.log(email, password)
+
+    if (!email || !password) {
+      throw new Error("missing fields or wrong password");
+
+    } else {
+      const data = await loginUser(email, password);
+      sessionStorage.setItem("token", data.accessToken);
+      sessionStorage.setItem("userId", data._id);
+         
+      window.location.href="./homeLogged.html";
+    }
+  })
+
+};
+
+solve();
--- Js Applications-Softuni/Data and Authentication - Exercise/06.Furniture/solution.js
+++ Js Applications-Softuni/Data and Authentication - Exercise/06.Furniture/solution.js
@@ -1,4 +1,27 @@
-function solve() {
+const logoutBtn = document.querySelector('#logoutBtn');
 
-  //TODO...
+async function logoutUser() {
+  const response = await fetch('http://localhost:3030/users/logout', {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": sessionStorage.getItem("token"),
+    },
+  })
 }
+
+
+function solve() {
+  logoutBtn.addEventListener('click', async (e) => {
+    if(sessionStorage.getItem("token")) {
+      logoutUser();
+      sessionStorage.clear();
+      window.location.href='./home.html';
+
+    } else {
+      throw new Error("No user token");
+    }
+  })
+};
+
+solve();
