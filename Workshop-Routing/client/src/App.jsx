fc0fc837e125e02d4fb03580e4c5dc6b2c219940
Emil Roydev <e.roydev@gmail.com>
2024-03-01 14:50:21 -0800

    authentication pages

--- Workshop-Routing/client/src/App.jsx
+++ Workshop-Routing/client/src/App.jsx
@@ -1,7 +1,12 @@
 import GameList from "./components/GameList/GameList";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import GameCreate from "./components/GameCreate/GameCreate";
+import Login from "./components/Login/Login";
+
+
 import { Routes, Route} from 'react-router-dom';
+import Register from "./components/Register/Register";
 
 function App() {
 
@@ -12,6 +17,9 @@ function App() {
       <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path='/games' element={<GameList />}></Route>
+          <Route path='games/create' element={<GameCreate />}></Route>
+          <Route path='login' element={<Login />}></Route>
+          <Route path='/register' element={<Register />}></Route>
       </Routes>
     </div>
   )
