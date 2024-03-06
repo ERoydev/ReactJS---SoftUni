bf56d6769e40eff77a606b8a307535bc8f40fe7e
Emil Roydev <e.roydev@gmail.com>
2024-03-06 14:47:00 -0800

    custom hooks

--- Workshop-Routing/client/src/components/Login/Login.jsx
+++ Workshop-Routing/client/src/components/Login/Login.jsx
@@ -1,30 +1,39 @@
 import { Link } from 'react-router-dom';
+import useForm from '../../hooks/useForm';
 
 export default function Login () {
-    return (
-        <section id="login-page" className="auth">
-  <form id="login">
-    <div className="container">
-      <div className="brand-logo" />
-      <h1>Login</h1>
-      <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Sokka@gmail.com"
-      />
-      <label htmlFor="login-pass">Password:</label>
-      <input type="password" id="login-password" name="password" />
-      <input type="submit" className="btn submit" value="Login" />
-      <p className="field">
-        <span>
-          If you don't have profile click <Link to="/register">here</Link>
-        </span>
-      </p>
-    </div>
-  </form>
-</section>
+  const {values, onChange, onSubmit} = useForm({
+    email: '',
+    password: '',
+  });
 
-    );
+  return (
+  <section id="login-page" className="auth">
+    <form id="login" onSubmit={onSubmit}>
+
+      <div className="container">
+        <div className="brand-logo" />
+          <h1>Login</h1>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Sokka@gmail.com"
+            onChange={onChange}
+            value={values.email}
+          />
+          <label htmlFor="login-pass">Password:</label>
+          <input type="password" id="login-password" name="password" value={values.password} onChange={onChange} />
+          <input type="submit" className="btn submit" value="Login" />
+          <p className="field">
+            <span>
+              If you don't have profile click <Link to="/register">here</Link>
+            </span>
+          </p>
+
+      </div>
+    </form>
+  </section>
+  );
 }
