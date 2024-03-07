d7e81018f86de1449d2690d93e7aab84d88849f8
Emil Roydev <e.roydev@gmail.com>
2024-03-07 15:48:48 -0800

    comments

--- Workshop-Routing/client/src/services/commentService.js
+++ Workshop-Routing/client/src/services/commentService.js
@@ -1,6 +1,18 @@
 import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/jsonstore/comments';
+const baseUrl = 'http://localhost:3030/data/comments';
+
+export const getAll = async (gameId) => {
+    const query = new URLSearchParams({
+        where: `gameId="${gameId}"`
+    });
+
+    const result = await request.get(`${baseUrl}?${query}`)
+
+    // TODO: Temporary solution. Can be optimized for better performance.
+    // return result.filter(comment => comment.gameId === gameId);
+    return result;
+}
 
 export const create = async (gameId, username, text) => {
     const newComment = await request.post(baseUrl, {
@@ -10,15 +22,4 @@ export const create = async (gameId, username, text) => {
     });
 
     return newComment;
-}
-
-export const getAll = async (gameId) => {
-    const query = new URLSearchParams({
-        where: `gameId="${gameId}"`
-    });
-
-    const result = await request.get(`${baseUrl}`)
-
-    // TODO: Temporary solution. Can be optimized for better performance.
-    return Object.values(result).filter(comment => comment.gameId === gameId);
 }
